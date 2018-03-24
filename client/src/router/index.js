b74a40f7c2910a26e51d8fe9f394cd972cc5db23
Gaoxufei <18047170938@163.com>
2018-03-24 20:51:55 +0800

    [ 2018.03.24 ] [ nav posts ]

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Index from '@/components/Index'
+
+import Index from '@/views/Index'
+import Posts from '@/views/posts'
 
 Vue.use(Router)
 
@@ -10,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'index',
       component: Index
+    },
+    {
+      path: '/posts',
+      name: 'posts',
+      component: Posts
     }
   ]
 })
---
+++ client/src/views/posts.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="posts">
+    <nav-header></nav-header>
+  </div>
+</template>
+
+<script>
+import NavHeader from '@/components/nav/nav'
+export default {
+  name: 'Posts',
+  components: {
+    NavHeader
+  },
+  data(){
+    return {
+      
+    }
+  },
+
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
